a409b5bcc7a557003911f6e19dfa2f82e7cfaae2
KaushalAgale <kaushalagale907@gmail.com>
2024-09-24 11:54:52 +0530

    push

--- dsa_in_c/Sorting & Searching/InsertionSort.c
+++ dsa_in_c/Sorting & Searching/InsertionSort.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-void accept(int *a, int n)
+
+void accept(int a[], int n)
 {
-    printf("jj");
     int i;
-    for(i=0; i<n; i++)
+    for(i=0;i<n;i++)
     {
         printf("Enter %d num : ",i+1);
         scanf("%d",&a[i]);
     }
 }
 
-void display(int *a,int n)
+void display(int a[],int n)
 {
     int i;
     for(i=0;i<n;i++)
     {
-        printf("%d",a[i]);
+        printf("%d ",a[i]);
     }
     printf("\n");
 }
 
-void inserting_sort(int *a,int n)
+void inserting_sort(int a[],int n)
 {
     int i,j,t;
     
     for(i=1;i<n;i++)
     {
         t=a[i];
-            for(j=j-1;j>=0;j--)
+            for(j=i-1;j>=0;j--)
             {
                 if(a[j]>t)
                 {
@@ -41,15 +41,14 @@ void inserting_sort(int *a,int n)
     }
 }
 
-int main() {
-
-    
-    int n=0;
-    
+int main() 
+{
+    int n;
     printf("Enter how many no. you want to sort : ");
     scanf("%d",&n);
+    int *a;
+    a = (int *) malloc(n * sizeof(int));
 
-    int *a = (int *) malloc(n * sizeof(int));
     if(a==NULL)
     {
         printf("Error While Making Array ");
@@ -57,11 +56,9 @@ int main() {
     }
 
     accept(a,n);
-
-    
-
-    inserting_sort(a,n);
     display(a,n);
-    
+    printf("After Sorting :");
+    inserting_sort(a,n);
+    display(a,n);    
     return 0;
 }
